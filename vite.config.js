67b238387b02a0a7def303e8635ce69cf4647b8e
Sujal Prajapati <sbprajapati310707@gmail.com>
2026-08-19 20:03:20 +0530

    Optimize Vite build target for Safari 13+ and add apple-touch-icon for iOS Safari normal tabs

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: ['es2015', 'edge88', 'firefox78', 'chrome87', 'safari13'],
+    cssTarget: ['chrome61', 'firefox60', 'safari11', 'edge18'],
+  },
 })
-
